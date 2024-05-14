580e63b3f218af269f1756b4b1a7ac987ed22a31
s12171934 <ssong99999@naver.com>
2024-05-14 20:32:22 +0900

    init

---
+++ src/main/java/com/solo/toyboard/ToyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.solo.toyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solo/toyboard/ToyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.toyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
